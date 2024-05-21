3ba7b5ebfc65e4c899dbbcf28e0fd119051bc9c4
Cheycops <nico.chacon99@gmail.com>
2024-05-21 18:48:41 -0500

    condiguracion de plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 /* -------------------------- import -------------------------- */
 import express from 'express';
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+/* manjeador de plantillas */
+import {engine} from 'express-handlebars'
+
+
 /* ----------------------------- initializacion ----------------------------- */
 const app = express();
+/*para evitar coliciones se colocan dos guiones ---- */
+const __dirname = dirname (fileURLToPath(import.meta.url));
+
 /* --------------------------------- setting -------------------------------- */
 app.set('port', process.env.PORT  || 3000 )
+app.set('views',join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials') ,
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
 /* ------------------------------- middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+/* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) =>{
+res.json({"message": "hola mundo"})
+});
 /* ------------------------------ public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')));
+
 /* ------------------------------- run server ------------------------------- */
 app.listen(app.get('port'), () =>{
     console.log('server listening on port',app.get('port'))
